71d14cf11f22ff01c0568da8240892899670843c
AmarAbdullaa <amarlareef01@gmail.com>
2024-11-14 22:02:52 +0530

    added

--- public/index.php
+++ public/index.php
@@ -1,1 +1,31 @@
-hello world
+<?php
+
+function show($stuff)
+{
+    echo "<pre>";
+    print_r($stuff);
+    echo "</pre>";
+}
+
+
+function splitURL()
+{
+    $URL = $_GET['url'] ?? 'home';
+    $URL = explode("/",$URL);
+    return $URL;
+}
+
+function loadContoller()
+{
+    $URL = splitURL();
+    $filename = "../app/controllers/".ucfirst($URL[0]).".php";
+    if(file_exists($filename))
+    {
+        require $filename;
+    }
+    else{
+        echo "Contller Not Found";
+    }
+}
+
+loadContoller();
