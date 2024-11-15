ff36ffc95b9298a8adc80bde337e938edb84cb2b
AmarAbdullaa <amarlareef01@gmail.com>
2024-11-15 11:23:47 +0530

    added

--- public/index.php
+++ public/index.php
@@ -1,31 +1,8 @@
 <?php
 
-function show($stuff)
-{
-    echo "<pre>";
-    print_r($stuff);
-    echo "</pre>";
-}
+session_start();
 
+require '../app/core/init.php';
 
-function splitURL()
-{
-    $URL = $_GET['url'] ?? 'home';
-    $URL = explode("/",$URL);
-    return $URL;
-}
-
-function loadContoller()
-{
-    $URL = splitURL();
-    $filename = "../app/controllers/".ucfirst($URL[0]).".php";
-    if(file_exists($filename))
-    {
-        require $filename;
-    }
-    else{
-        echo "Contller Not Found";
-    }
-}
-
-loadContoller();
+$app = new App;
+$app->loadController();
