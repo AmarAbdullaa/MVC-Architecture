e74dbc16c541dcabb47e90dfa183a58ee1d305bd
AmarAbdullaa <amarlareef01@gmail.com>
2024-11-15 15:19:23 +0530

    added

---
+++ app/core/config.php
@@ -0,0 +1,11 @@
+<?php
+
+if($_SERVER['SERVER_NAME'] == 'localhost')
+{
+    define('ROOT','http://localhost/MVC/public');
+
+}
+else
+{
+    define('ROOT','https://www.yourwebsite.com');
+}
--- app/views/home.view.php
+++ app/views/home.view.php
@@ -1,3 +1,3 @@
 <h1>Home Page View</h1>
 
-<img src="assets/images/rota" >
+<img src="<?= ROOT?>/assets/images/rota.jpeg" >
